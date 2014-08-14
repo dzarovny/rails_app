f6cf775c8e11909f8610a8e74a7c9753ea2d69fe
Dave <david.zarovny@gmail.com>
2014-08-13 23:20:14 -0700

    working on parallax and updated post contents to be a Check-list

--- rails_app/db/schema.rb
+++ rails_app/db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813215024) do
+ActiveRecord::Schema.define(version: 20140813225753) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
-    t.text     "content"
+    t.text     "body"
+    t.boolean  "published"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
